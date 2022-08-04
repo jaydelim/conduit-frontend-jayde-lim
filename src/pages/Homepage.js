dda5d806a5c23416425323e22d7f3edb0a5c0549
Jayde Lim <103135192+jaydelim@users.noreply.github.com>
2022-08-04 08:38:51 -0400

    feat: create Navigation Bar (#2)
    * feat: create Header component
    
    * feat: create Body component
    
    * feat: present Header and Body component within app
    
    * fix: modify mobile layout in Body component
    
    * fix: modify mobile layout in Header component
    
    * refactor: create Homepage component and restructure
    
    * refactor: restructure components
    
    * refactor: restructure files and folders
    
    * refactor: restructure into explicit components and their containers
    
    * refactor: rename components and corresponding file-name
    
    * feat: create navigation bar

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import HomepageBanner from '../components/HomepageBanner';
 import TagsSidebar from "../components/TagsSidebar";
 import MainFeedBoard from "../components/MainFeedBoard";
+import NavBar from"../components/NavBar";
 
 function Homepage() {
   return (
     <div>
+      <NavBar/>
       <HomepageBanner/>
 
       <div className="flex flex-col sm:flex-row-reverse items-start h-screen pt-10 min-w-[456px] ml-4 mr-4">
